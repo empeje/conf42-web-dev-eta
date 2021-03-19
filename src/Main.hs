c56f355e227e1babd38771c5ae4f9e54f9d0d39d
mpj <abdurrachman.mappuji@mekari.com>
2021-03-20 00:22:32 +0700

    Try to follo Fibonacci API

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,33 @@ module Main where
 
 import Primes
 import Java
+import Servant
+import Servant.Server
+import Data.Aeson
+import Network.Wai.Servlet.Handler.Jetty
+
+type API = "fibonacci" :> Capture "n" Int :> Get '[JSON] Value
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+api :: Proxy API
+api = Proxy
+
+server :: Application
+server = serve api fibHandler
+  where fibHandler n
+          | n >= 0
+          = return $ object ["n" .= n, "fib" .= fib n]
+          | otherwise = throwError $ err412 { errBody = "Precondition Failed: n >= 0" }
+
+main :: IO ()
+main = run 9000 server
 
 -- main = putStrLn $ "The 101st prime is " ++ show (primes !! 100)
-main = putStrLn $ show (xor True False)
+-- main = putStrLn $ show (xor True False)
 
 fullName :: String
 fullName = "Harry Potter"
